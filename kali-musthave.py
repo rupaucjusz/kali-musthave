c9eb008a31a43b4f517a28c4705e322bab81daa8
Elliot <elliot@fsociety>
2019-01-09 15:24:47 -0500

    not finished

---
+++ kali-musthave.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+import os, time
+print('\033[96m Hello in kali musthave\033[0m')
+print("..............
+            ..,;:ccc,.
+          ......''';lxO.
+.....''''..........,:ld;
+           .';;;:::;,,.x,
+      ..'''.            0Xxoc:,.  ...
+  ....                ,ONkc;,;cokOdc',.
+ .                   OMo           ':ddo.
+                    dMc               :OO;
+                    0M.                 .:o.
+                    ;Wd
+                     ;XO,
+                       ,d0Odlc;,..
+                           ..',;:cdOOd::,.
+                                    .:d;.':;.
+                                       'd,  .'
+                                         ;l   ..
+                                          .o
+                                            c
+                                            .'
+                                             .
+")
