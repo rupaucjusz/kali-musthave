b3a9bc0c8b3b210a8b7b6c9ad81885ad9cd5d032
Retr0 <pentest.lamusy@gmail.com>
2019-01-11 00:58:12 -0500

    new features

--- kali-musthave.py
+++ kali-musthave.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python2
 import os, time
-print('\033[96m Hello in kali musthave kali rules \033[0m\n\nso first update to latest kali')
+print('\033[96m Hello in kali musthave kali rules \033[0m\n\nso first update kali')
+def repofix():
+    version = raw_input('but before update do you have the newest kali ? y/n')
+    os.system(''' # Regular repositories
+deb http://http.kali.org/kali sana main non-free contrib
+deb http://security.kali.org/kali-security sana/updates main contrib non-free
+# Source repositories
+deb-src http://http.kali.org/kali sana main non-free contrib
+deb-src http://security.kali.org/kali-security sana/updates main contrib non-free > /etc/apt/sources.list''')
+if version == "y":
+    repofix()
+elif version == "n":
+    repos = raw_input('so tell me if you have 2.x or 1.x kali (check at kali download page if higher then 2.x type 2.x)')
+    if repos == "2.x":
+        repofix()
+    elif repos == "1.x":
+        os.system("""echo '## Regular repositories
+deb http://http.kali.org/kali kali main non-free contrib
+deb http://security.kali.org/kali-security kali/updates main contrib non-free
+## Source repositories
+deb-src http://http.kali.org/kali kali main non-free contrib
+deb-src http://security.kali.org/kali-security kali/updates main contrib non-free' > /etc/apt/sources.list""")
+
 print('ignore apt warnings')
 time.sleep(5)
 os.system('apt update > /dev/null && apt upgrade -y > /dev/null && apt dist-upgrade -y > /dev/null && apt autoremove -y > /dev/null && apt autoclean && apt clean')
@@ -8,5 +30,24 @@ print('ok')
 print('now some tools')
 os.system('apt install -y git python3 python3-pip build-essential > /dev/null')
 os.system('git clone https://github.com/arismelachroinos/lscript && cd lscript && chmod +x install.sh && ./install.sh')
-os.system('apt install -y aircrack-ng websploit routersploit hashcat ncat nmap zenmap lynis debsums debsecan clamav needrestart fail2ban apt-listbugs apt-listchanges debian-goodies')
-print('done\nok have fun')
+os.system('apt install -y aircrack-ng mdk3 pyrit alsa-utils cowpatty terminator websploit hydra metasploit-framework armitage routersploit hashcat tor proxychains ncat nmap zenmap lynis debsums debsecan clamav needrestart fail2ban apt-listbugs apt-listchanges debian-goodies')
+print('done')
+print('now this script will create a function to your system\nwhichupdates kali')
+os.system("""function apt-updater {
+	apt-get update &&
+	apt-get dist-upgrade -Vy &&
+	apt-get autoremove -y &&
+	apt-get autoclean &&
+	apt-get clean &&
+	reboot
+	}""")
+print('type "apt-updater" any time to check and update system')
+print('some bugs kali have now will be fixed...')
+os.system('''echo "
+[main]
+plugins=ifupdown,keyfile
+
+[ifupdown]
+managed=true
+"> /etc/NetworkManager/NetworkManager.conf''')
+print('now have fun')
